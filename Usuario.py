3ac6ef538c60a0d0c8b864465f38f2954d9cc665
FranGuh <pedicles62900@gmail.com>
2024-11-03 16:41:59 -0600

    version 1.0.2 Corregido un bug

--- Usuario.py
+++ Usuario.py
@@ -6,7 +6,7 @@ class UsuarioInterfaz:
     def __init__(self, root):
         self.root = root
         self.root.title("Interfaz Usuario")
-        self.root.geometry("400x300")
+        self.center_window(400, 300)  # Tamaño de la ventana 400x300
 
         # Etiqueta de título
         title_label = tk.Label(root, text="Interfaz Usuario", font=("Helvetica", 16))
@@ -24,19 +24,27 @@ class UsuarioInterfaz:
         btn_regresar = tk.Button(root, text="Regresar", command=self.root.destroy)
         btn_regresar.pack(pady=10)
 
+    def center_window(self, width, height):
+        # Obtener el tamaño de la pantalla
+        screen_width = self.root.winfo_screenwidth()
+        screen_height = self.root.winfo_screenheight()
+
+        # Calcular posición x e y para centrar la ventana
+        x = (screen_width // 2) - (width // 2)
+        y = (screen_height // 2) - (height // 2)
+
+        # Establecer tamaño y posición de la ventana
+        self.root.geometry(f"{width}x{height}+{x}+{y}")
+
     def abrir_busqueda_sintomas(self):
-        ####
         ejecutar_BusquedaSintoma()
+
     def busqueda_enfermedad(self):
-        # Método placeholder para "Búsqueda por enfermedad"
-        # Aquí puedes implementar la funcionalidad en el futuro
         messagebox.showinfo("Funcionalidad no implementada", "Esta función aún no está disponible.")
-        
+
 def ejecutar_BusquedaSintoma():
-    subprocess.run(["python", "BusquedaSintoma.py"])  # Ejecuta relacion.py
+    subprocess.run(["python", "BusquedaSintoma.py"])
 
-
-# Ejemplo de función para mostrar la interfaz en una ventana nueva
 def mostrar_interfaz_usuario():
     root = tk.Tk()
     app = UsuarioInterfaz(root)
